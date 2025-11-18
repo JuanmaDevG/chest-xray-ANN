180ec49788ceb90e7427f83a5b092ff0f5664363
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2025-11-18 17:36:14 +0100

    Some advancements in learning the api and I added the templates to arease them later

--- examples/sequential.py
+++ examples/sequential.py
@@ -1,3 +1,24 @@
+# Trying the sequential api
+
 import numpy as np
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
+import random
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
+
+    my_model = keras.Sequential([
+        layers.Input(shape=(28,28)),
+        layers.Rescaling(scale=1./255),
+        layers.Flatten(),
+        layers.Dense(256),
+        layers.Dense(256),
+        layers.Dense(10, activation='softmax')
+        ])
+    
+    my_model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    my_model.fit(X_train, y_train, epochs=5, verbose=1)
